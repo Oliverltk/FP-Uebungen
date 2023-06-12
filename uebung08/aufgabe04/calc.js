39c75c7a642ed57385093d251144502a1f581e0f
Oliver <oliver.litke2002@gmail.com>
2023-06-12 15:10:58 +0200

    Übung08 Aufgabe04

---
+++ uebung08/aufgabe04/calc.js
@@ -0,0 +1,81 @@
+// HTTP Modul einbinden
+var http = require("http");
+
+// URL Modul einbinden
+var url = require("url");
+
+// HTTP Server erzeugen
+var server = http.createServer(serve);
+
+// Server an Port binden
+server.listen(3000);
+
+// Funktion des HTTP-Servers
+// req: Anfrage vom Client (Request)
+// res: Antwort des Servers (Response)
+function serve(req, res) {
+  let parsedUrl = url.parse(req.url, true);
+  let path = parsedUrl.pathname;
+  let parts = path.split("/");
+
+  if (parts.length !== 4) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("Fehler: Ungültige Anfrage.");
+    return;
+  }
+
+  let operator = parts[1];
+  let operand1 = parseFloat(parts[2]);
+  let operand2 = parseFloat(parts[3]);
+
+  if (isNaN(operand1) || isNaN(operand2)) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("Fehler: Operanden müssen numerisch sein.");
+    return;
+  }
+
+  let result = null;
+
+  switch (operator) {
+    case "add":
+      result = operand1 + operand2;
+      break;
+    case "sub":
+      result = operand1 - operand2;
+      break;
+    case "mul":
+      result = operand1 * operand2;
+      break;
+    case "div":
+      if (operand2 === 0) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Fehler: Division durch 0 nicht möglich");
+        return;
+      }
+      result = operand1 / operand2;
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Fehler: Nicht unterstützter Operator.");
+      return;
+  }
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(result.toString());
+  /*
+  if (query.order && responses.order[query.order]) {
+    let response = responses.order[query.order];
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(response);
+  } else if (query.location && responses.location[query.location]) {
+    let response = responses.location[query.location];
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(response);
+  } else if (query.starwars && responses.starwars[query.starwars]) {
+    let response = responses.starwars[query.starwars];
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(response);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Fehler: Nicht unterstützte Kategorie oder Stichwort.");
+  }*/
+}
